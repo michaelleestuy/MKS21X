2c92033fa2cc9bf80703dbffdab06ecb5c1fa820
Michael Lee <michaelleestuy@gmail.com>
2016-11-30 06:30:09 -0500

    changed main

--- 01Library/Library.java
+++ 01Library/Library.java
@@ -74,6 +74,15 @@ public class Library {
 			book.returned ();
 	}
 
+
+
+
+
+
+
+
+
+
 	/**
 	 * main testing program
 	 * @param args  not used
@@ -83,37 +92,13 @@ public class Library {
 		Library lib = new Library ();
 
 		// set up library
-		lib.addBook(new ReferenceBook ("Henry M. Walker",
-					"Problems for Computer Solution using BASIC",
-					"0-87626-717-7", "QA76.73.B3W335", "Iowa Room"));
-		lib.addBook(new ReferenceBook ("Samuel A. Rebelsky",
-				"Experiments in Java",
-				"0201612674", "64.2 R25ex", "Iowa Room"));
-		lib.addBook(new CirculatingBook ("John David Stone",
-				"Algorithms for functional programming",
-				"in process", "forthcoming"));
-		lib.addBook(new CirculatingBook ("Henry M. Walker",
-				"Computer Science 2:  Principles of Software Engineering, Data Types, and Algorithms",
-				"0-673-39829-3", "QA76.758.W35"));
+		lib.addBook(new ReferenceBook("Ur Mom", "Ur Life", "ISBN:10101010101", "CallNumber: 102.192", "Ur House"));
+		lib.addBook(new ReferenceBook("Ur Dad", "Ur Lives", "ISBN:10101010102", "CallNumber: 102.193", "Ur Room"));
+		lib.addBook(new ReferenceBook("Ur Cat", "Ur Childhood", "ISBN:10101010103", "CallNumber: 102.194", "Ur House's House'"));
 
-		lib.addBook(new CirculatingBook ("Henry M. Walker",
-				"Problems for Computer Solution using FORTRAN",
-				"0-87626-654-5", "QA43.W34"));
-		lib.addBook(new CirculatingBook ("Henry M. Walker",
-				"Introduction to Computing and Computer Science with Pascal",
-				"0-316-91841-5", "QA76.6.W3275"));
-		lib.addBook(new CirculatingBook ("Samuel A. Rebelsky and Philip Barker",
-				"ED-MEDIA 2002 : World Conference on Educational Multimedia, Hypermedia & Telecommunications",
-				"14. 1-880094-45-2", "64.2 25e"));
-		lib.addBook(new CirculatingBook ("Henry M. Walker",
-				"Pascal:  Problem Solving and Structured Program Design",
-				"0-316-91848-2", "QA76.73.P2W35"));
-		lib.addBook(new CirculatingBook ("Henry M. Walker",
-			    "The Limits of Computing",
-			    "0-7637-2552-8", "QA76.W185"));
-		lib.addBook(new CirculatingBook ("Henry M. Walker",
-			    "The Tao of Computing",
-			    "0-86720-206-8", "QA76.W1855"));
+		lib.addBook(new CirculatingBook("Michael Lee", "Eugene's Life'", "ISBN:199", "CallNumber: 222.192"));
+		lib.addBook(new CirculatingBook("Michael ShLee", "ShEugene's Life'", "ISBN:1991", "CallNumber: 222.193"));
+		lib.addBook(new CirculatingBook("ShMichael Lee", "Eugene's ShLife'", "ISBN:1992", "CallNumber: 222.194"));
 
 		// sort books by call number
 		lib.sortLibrary();
@@ -122,18 +107,12 @@ public class Library {
 		lib.printLibrary();
 
 		// some users check out and return books
-		lib.checkout("Donald Duck", "March 1, 2012", "QA43.W34");
-		lib.checkout("Donald Duck", "March 12, 2012", "QA76.6.W3275");
-		System.out.print("FAIL: ");
-		lib.checkout("Donald Duck", "March 6, 2012", "64.2 R25ex");
-		lib.checkout("Minnie Mouse", "April 1, 2012", "64.2 25e");
+		lib.checkout("Henry Thoreau", "Tomorrow", "CallNumber: 102.193");
+		lib.checkout("Emerson Henry Thoreau", "Day After Tomorrow", "CallNumber: 222.193");
+		lib.checkout("Emerson Henry Thoreau Ralph", "Day After Day After Tomorrow", "CallNumber: 222.194");
+		lib.returned("CallNumber: 222.193");
 
-		System.out.print("FAIL: ");
-		lib.checkout("Goofy", "February 28, 2012", "12345");
-		lib.returned("QA76.6.W3275");
-		System.out.print("FAIL: ");
-		lib.returned("64.2 R25ex");
-		lib.checkout("Goofy", "March 28, 2012", "QA76.6.W3275");
+
 		// print final status of library
 		lib.printLibrary();
 	}
