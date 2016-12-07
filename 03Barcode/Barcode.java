f93dcfe5e4c7e809d11c1faa735f93cc16185b5b
Michael Lee <michaelleestuy@gmail.com>
2016-12-07 06:33:24 -0500

    first steps

---
+++ 03Barcode/Barcode.java
@@ -0,0 +1,39 @@
+public class Barcode implements Comparable<Barcode>{
+    private String _zip;
+    private int _checkDigit;
+    
+
+
+    private static final String one = ":::||";
+    private static final String two = "::|:|";
+    private static final String three = "::||:";
+    private static final String four = ":|::|";
+    private static final String five = ":|:|:";
+    private static final String six = ":||::";
+    private static final String seven = "|:::|";
+    private static final String eight = "|::|:";
+    private static final String nine = "|:|::";
+    private static final String zero = "||:::";
+
+
+    public Barcode(String zip){
+	if(zip.length() != 5){
+	    RuntimeException e = new RuntimeException("Incorrect barcode length");
+	    throw e;
+	}
+	_zip = zip;
+    }
+    
+    public int compareTo(Barcode other){
+	return 7;
+    }
+
+    private int checkSum(){
+	int sum = 0;
+        for(int i = 0; i < 5; i++){
+	    sum += (int)(_zip.charAt(i));
+	}
+	return sum % 10;
+    }
+
+}
