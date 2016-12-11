879f0f407233b4b891f71fc0c612551f80566786
Michael Lee <michaelleestuy@gmail.com>
2016-12-11 18:28:10 -0500

    finished toZip

--- 03Barcode/Barcode.java
+++ 03Barcode/Barcode.java
@@ -1,8 +1,8 @@
-
+import java.util.*;
 
 public class Barcode implements Comparable<Barcode>{
     private String _zip;
-    public static final String[]values = {"||:::", //0
+    private static final String[]values = {"||:::", //0
 					  ":::||", //1
 					  "::|:|", //2
 					  "::||:", //3
@@ -14,6 +14,46 @@ public class Barcode implements Comparable<Barcode>{
 					  "|:|::", //9
     };
 
+    
+    
+    public static String toZip(String code){
+	if(code.length() != 32){
+	    throw new IllegalArgumentException("Incorrect length of code");
+	}
+	
+	for(int i = 0; i < 32; i++){
+	    if(!(code.charAt(i) == ':' || code.charAt(i) == '|')){
+		throw new IllegalArgumentException("Invalid characters in code");
+	    }
+	}
+
+	if(!(code.charAt(0) == '|' && code.charAt(31) == '|' )){
+	    throw new IllegalArgumentException("Guard rails missing");
+	}
+
+	String nums = "";
+	for(int i = 0; i < 6; i++){
+	    String b = code.substring(i * 5 + 1, i * 5 + 6);
+	    int c = Arrays.asList(values).indexOf(b);
+	    if(c == -1){
+		throw new IllegalArgumentException("Invalid encoded ints");
+	    }
+	    nums += c;
+	}
+
+	int sum = 0;
+	for(int i = 0; i < 5; i++){
+	    sum += Integer.parseInt("" + nums.charAt(i));
+	}
+	if(sum % 10 != Integer.parseInt("" + nums.charAt(5))){
+	    throw new IllegalArgumentException("Check Digit is wrong");
+	}
+
+	return nums.substring(0, 5);
+	
+    }
+    
+    
     public static String toCode(String zip){
 	if (zip.length() != 5){
 	    throw new IllegalArgumentException("Incorrect zip length"); 
@@ -88,17 +128,18 @@ public class Barcode implements Comparable<Barcode>{
     }
 
     public static void main(String[]args){
-	
+	/*
 	Barcode a = new Barcode("41824");
 	System.out.println(a);
 	Barcode b = new Barcode("18249");
 	System.out.println(b);
-	/*
+	
 	System.out.println(a.compareTo(b));
 	System.out.println(b.compareTo(a));
 	*/
 	//System.out.println(toCode("47162"));
-	
+	//System.out.println(toCode("41828"));
+	//System.out.println(toZip("||||||||||||||||||||||||||||||||"));
     }
 
     
