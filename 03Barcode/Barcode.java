ecafc66851b83770f5cefd965076400965aee740
Michael Lee <michaelleestuy@gmail.com>
2016-12-11 17:31:12 -0500

    constructor finished

--- 03Barcode/Barcode.java
+++ 03Barcode/Barcode.java
@@ -1,40 +1,34 @@
-public class Barcode implements Comparable<Barcode>{
+public class Barcode{
     private String _zip;
-    private int _checkDigit;
-    
-
-
-    private static final String one = ":::||";
-    private static final String two = "::|:|";
-    private static final String three = "::||:";
-    private static final String four = ":|::|";
-    private static final String five = ":|:|:";
-    private static final String six = ":||::";
-    private static final String seven = "|:::|";
-    private static final String eight = "|::|:";
-    private static final String nine = "|:|::";
-    private static final String zero = "||:::";
 
 
     public Barcode(String zip){
-	if(zip.length() != 5){
-	    RuntimeException e = new RuntimeException("Incorrect barcode length");
-	    throw e;
+	if (zip.length() != 5){
+	    throw new IllegalArgumentException("Incorrect zip length"); 
+	}
+
+	for(int i = 0; i < 5; i++){
+	    if (!(zip.charAt(i) >= '0' && zip.charAt(i) <= '9')){
+		    throw new IllegalArgumentException("Incorrect Non digit inside zip");
+		}
 	}
 	_zip = zip;
-	_checkDigit = this.checkSum();
+
+	int a = 0;
+	for(int i = 0; i < 5; i++){
+	    a += zip.charAt(i);
+	}
+	_zip += (a % 10);
+	
+    }
+
+    public String toString(){
+	return _zip;
+    }
+
+    public static void main(String[]args){
+	Barcode a = new Barcode("41824");
+	System.out.println(a);
     }
     
-    public int compareTo(Barcode other){
-	return 7;
-    }
-
-    private int checkSum(){
-	int sum = 0;
-        for(int i = 0; i < 5; i++){
-	    sum += (int)(_zip.charAt(i));
-	}
-	return sum % 10;
-    }
-
 }
