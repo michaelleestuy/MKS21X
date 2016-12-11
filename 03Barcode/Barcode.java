ce8af476ce08a7ea18b9fcdb2176a91110159e1b
Michael Lee <michaelleestuy@gmail.com>
2016-12-11 17:40:23 -0500

    finished compareTo

--- 03Barcode/Barcode.java
+++ 03Barcode/Barcode.java
@@ -1,5 +1,25 @@
-public class Barcode{
+public class Barcode implements Comparable<Barcode>{
     private String _zip;
+    private String[]values = {"||:::", //0
+			      ":::||", //1
+			      "::|:|", //2
+			      "::||:", //3
+			      ":|::|", //4
+			      ":|:|:", //5
+			      ":||::", //6
+			      "|:::|", //7
+			      "|::|:", //8
+			      "|:|::", //9
+    };
+
+    public String getZip(){
+	return _zip;
+    }
+
+    public int compareTo(Barcode other){
+	return (this.getZip()).compareTo(other.getZip());
+	
+    }
 
 
     public Barcode(String zip){
@@ -29,6 +49,10 @@ public class Barcode{
     public static void main(String[]args){
 	Barcode a = new Barcode("41824");
 	System.out.println(a);
+	Barcode b = new Barcode("18249");
+	System.out.println(b);
+	System.out.println(a.compareTo(b));
+	System.out.println(b.compareTo(a));
     }
     
 }
