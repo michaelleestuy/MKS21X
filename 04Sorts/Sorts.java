aca456d89609564229b1a55e8e06ccf0993b8c51
Michael Lee <michaelleestuy@gmail.com>
2016-12-13 06:19:41 -0500

    swapper helper

---
+++ 04Sorts/Sorts.java
@@ -0,0 +1,24 @@
+public class Sorts{
+    
+    /**EDIT THIS METHOD TO MATCH YOUR NAME 
+     */
+    public static String name(){
+	return "10.Lee.Michael";
+    }
+    
+    public static void swap(int[]ary, int a, int b){
+	int c = ary[b];
+	ary[b] = ary[a];
+	ary[a] = c;
+    }
+    
+    /**Selection sort of an int array. 
+     *Upon completion, the elements of the array will be in increasing order.
+     *@param data  the elements to be sorted.
+     */
+    public static void selectionSort(int[]data){
+	
+	
+    }
+    
+}
