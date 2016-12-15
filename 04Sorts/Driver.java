65c146f7a0118689fc102f2072b9666716e4e63c
Michael Lee <michaelleestuy@gmail.com>
2016-12-15 06:38:18 -0500

    incomplete timer

---
+++ 04Sorts/Driver.java
@@ -0,0 +1,25 @@
+import java.io.*
+
+
+public class Driver{
+
+    public static void main(String[]args){
+	/*
+	  0 - selection
+	  1 - insertion
+	  2 - bubble
+	*/
+
+	int a = Integer.parseInt(args[0]);
+	int[]acorn = new int[a];
+
+	for(int i = 0)
+
+
+
+
+    }
+
+
+
+}
