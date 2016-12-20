a61208c14d9c4bc39a69f694f63c5a4b241ae9d8
Michael Lee <michaelleestuy@gmail.com>
2016-12-20 06:14:35 -0500

    ftoc and ctof

---
+++ Temps.java
@@ -0,0 +1,11 @@
+public class Temps{
+    public static double CtoF(double t){
+	return t * 1.8 + 32;
+    }
+    
+    public static double FtoC(double t){
+	return (t - 32) * 5 / 9;
+    }
+    
+
+}
